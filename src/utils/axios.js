db6ee9ef685c8b92a958c7392d9291ba90cba976
andres <andres.david.mm@hotmail.com>
2024-03-21 16:01:21 -0500

    added error handling

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,8 @@
 import axiosDefault from 'axios';
 import store from '../store';
+import router from '../router';
+import { toast } from 'react-toastify';
+import { logout } from '../store/slices/app.slice';
 
 const axios = axiosDefault.create({
     baseURL: 'https://hotels-api.academlo.tech'
@@ -12,5 +15,28 @@ axios.interceptors.request.use(function (config) {
     }
     return config;
 })
+
+axios.interceptors.response.use(null, (error) => {
+    console.log(error);
+    if (error.response.status === 403) {
+        store.dispatch(logout());
+        router.navigate('/login');
+        toast('Session expired. You have to login again', {
+            theme: 'dark',
+            type: 'error'
+        })
+        return;
+    }
+    if (error.response?.data?.message?.includes('Invalid credentials')) {
+        return Promise.reject(error);
+    }
+    if (error.response?.data?.message?.includes('Validation error')) {
+        return Promise.reject(error);
+    }
+    toast('There was an error', {
+        theme: 'dark',
+        type: 'error'
+    })
+});
 
 export default axios;
